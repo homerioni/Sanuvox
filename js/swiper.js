0cfaa21e313a1cf0a29a1c53b1e0acb3c9a5238a
homerioni <homerion13@gmail.com>
2022-03-16 21:03:57 +0300

    create section intro and about-intro

--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,6 @@
+// Пересчет rem в px для swiper
 const rem = function (rem) {
-    if ($(document).width() > 768) {
+    if ($(window).width() > 768) {
         return 0.005208335 * $(window).width() * rem;
     } else {
         // где 375 это ширина моб версии макета
@@ -62,5 +63,66 @@ const reviews_slider = new Swiper('.reviews__slider', {
         autoplay: true,
         delay: 7000,
         disableOnInteraction: true,
+    },
+});
+
+const intro_slider = new Swiper('.intro__swiper', {
+    direction: 'vertical',
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.intro__pagination',
+        type: 'fraction',
+        formatFractionCurrent: function (number) {
+            if (number < 10) {
+                return '0' + number;
+            } else {
+                return number;
+            }
+        },
+        formatFractionTotal: function (number) {
+            if (number < 10) {
+                return '0' + number;
+            } else {
+                return number;
+            }
+        },
+        renderFraction: function (currentClass, totalClass) {
+            return '<div class="' + currentClass + '"></div>' +
+                '<div class="' + totalClass + '"></div>';
+        },
+    },
+
+    navigation: {
+        nextEl: '.intro__arrow--bottom',
+        prevEl: '.intro__arrow--top',
+    },
+
+    autoplay: {
+        autoplay: true,
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+});
+
+const about_intro_slider = new Swiper('.about-intro__slider', {
+    direction: 'horizontal',
+    slidesPerView: 4,
+
+    breakpoints: {
+        769: {
+            spaceBetween: rem(10.3),
+        },
+
+        0: {
+            slidesPerView: 1.4,
+            spaceBetween: rem(2),
+            loop: true,
+            autoplay: {
+                autoplay: true,
+                delay: 5000,
+                disableOnInteraction: true,
+            },
+        },
     },
 });
